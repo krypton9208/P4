b2b1881ef86c78d66ec5302f0093b2b424f4db0f
krypton9208 <krypton9208@gmail.com>
2014-03-09 09:29:01 +0100

    AFTER WYKŁAD

--- Z2/Program.cs
+++ Z2/Program.cs
@@ -10,6 +10,51 @@ namespace Z2
     {
         static void Main(string[] args)
         {
+            using (var context = new School1Entities)
+            {
+                foreach (var item in Context.poepole
+	            {
+		            Console.WriteLine("cos tam coś tam z bazy");
+                    if (item is Student)
+                    {
+                        if (item is Bussinesstudent)
+                        {
+                            Console.WriteLine({"0}", (item as Bussinessstudent.Credints);
+
+                        }
+                        else if (item is Instructor)
+                        {
+                            Console.WriteLine(item.date);
+                        }
+                    }
+	            }
+
+                foreach (var item in context.Peopole.ofType<BussinessStudent>))
+                {
+                    Console.WriteLine(item.lastname, item.Credits);
+                }
+	        {
+                
+		            //CRUD - create select update delete
+                    BussinessStudent bs = new BussinessStudent
+                    {
+                        department = "other", 
+                        credits = 1800,
+                        firstname = "paweł"
+                        lastname="siemienik"
+                    };
+                    context.people.Add(bs);
+                    context.saveChanges();
+                    
+                    foreach (var item in context.)
+
+	        }
+            }
+        
+	{
+		 
+	}
+
         }
     }
 }
