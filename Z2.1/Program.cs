4677a20979c0ffe17a9339d35a9f19af8a76db35
krypton9208 <krypton9208@gmail.com>
2014-03-09 10:58:02 +0100

    Wyświetla Numer klienta oraz nazwe, a potem pokazuje nazwe produktu które kupił

---
+++ Z2.1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Z2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new DataClasses1DataContext())
+            {
+               // context.Log = Console.Out;
+                var suppliers = context.Products.GroupBy(
+                    x => x.Supplier
+                    );
+
+                foreach (var item in suppliers)
+                {
+                    Console.WriteLine("{0} {1}",item.Key.SupplierID, item.Key.CompanyName);
+                    foreach (var product in item)
+                    {
+                        Console.WriteLine("   - {0}", product.ProductName);     
+                    }
+                }
+            }
+
+        }
+    }
+}
