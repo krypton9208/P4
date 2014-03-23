fd7ee4707400bde62a6dbe46a6417511afd8f819
krypton9208 <krypton9208@gmail.com>
2014-03-23 10:33:43 +0100

    ...

---
+++ Z2.3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace Z2._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using( var context = new school1Ent())
+            {
+                foreach (var instructor in context.People.OfType<Instructor>())
+                {
+                    Console.WriteLine("{0}{1}", instructor.FirstName);
+                }
+            }
+
+        }
+    }
+}
