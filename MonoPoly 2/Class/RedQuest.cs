c42b618554022606d3335ad2d6bfb875e4ad38c1
krypton9208 <krypton9208@gmail.com>
2014-09-16 18:41:29 +0200

    other

---
+++ MonoPoly 2/Class/RedQuest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonoPoly_2.Class
+{
+    class RedQuest : Pole
+    {
+        private string Text { get; set; }
+        public QuestType Type { get; set; }
+        private int Param;
+        public RedQuest (string text, QuestType type, int param, string name, int pos)
+        {
+            Text = text;
+            Type = type;
+            Param = param;
+            Name = name;
+            Cost = 0;
+            Pos = pos;
+        }
+    }
+}
